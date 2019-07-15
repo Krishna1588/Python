6058336f29e509f77b1df746882220af41df44d1
Krishna1588 <52617727+Krishna1588@users.noreply.github.com>
2019-07-15 06:22:13 +0530

    Add files via upload

--- Reverse_string_pattern.py
+++ Reverse_string_pattern.py
@@ -1,7 +1,15 @@
+#Forward String Pattern
+
+s="Krishna"
+s1=''
+for c in s:
+    s1 = s1 + c  # appending chars in reverse order
+    print(s1)
+
 #Reverse String Pattern
 
 s="Krishna"
 s1=''
 for c in s:
-    s1 = c + s1  # appending chars in reverse order
+    s1 = c + s1 # appending chars in reverse order
     print(s1)
